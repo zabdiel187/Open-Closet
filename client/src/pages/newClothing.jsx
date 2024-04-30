9263633356f862188bcc8427b733e5258197d0d8
zabdiel187 <zabdieljoseph187@gmail.com>
2024-04-29 23:24:38 -0400

    yeet init

--- client/src/pages/newClothing.jsx
+++ client/src/pages/newClothing.jsx
@@ -1,6 +1,15 @@
 import "./newClothing.css";
+import { useState } from "react";
 
 const NewClothing = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [brand, setBrand] = useState("");
+  const [size, setSize] = useState("");
+  const [rentalPrice, setRentalPrice] = useState("");
+  const [retailPrice, setRetailPrice] = useState("");
+  const [color, setColor] = useState("");
+
   return (
     <div className="newClothingComponent">
       <h1> Create New Item</h1>
@@ -8,9 +17,9 @@ const NewClothing = () => {
         <input type="text" placeholder="Name" className="" />
         <input type="text" placeholder="Description" className="" />
         <input type="text" placeholder="Brand" className="" />
+        <input type="text" placeholder="Size" className="" />
         <input type="text" placeholder="Rental Price" className="" />
         <input type="text" placeholder="Retail Price" className="" />
-        <input type="text" placeholder="Name" className="" />
         <input type="text" placeholder="Color" className="" />
         <input type="text" placeholder="Min Rental Period" className="" />
       </div>
